95018684c8a6a390419ef88a724883af19e0af6c
Joe Gesualdo <joegesualdo@gmail.com>
2012-11-28 20:48:05 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homespreelandingpage::Application.config.secret_token = 'aa662682ee98b5777aa030e2d80249bbc866099dfaaea1ea637de07d93058f7e41cb4bd1c8f1a9f8d81c6661d7e161cbd3284cdfba09b92e5b3c5322c12652dc'
